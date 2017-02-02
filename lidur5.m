6e3e0ea7964957bd0377baaad0ce99dcf1576f44
alivarsdotttir <alexandralif@hotmail.com>
2017-02-02 11:21:54 +0000

    adge

--- lidur5.m
+++ lidur5.m
@@ -11,22 +11,22 @@ x2 = 0;
 y2 = 6;
 fn=@(theta) ((y2 - L2*sin(gamma + theta))*(L3^2*sin(theta)^2 + (x1 - L3*cos(theta))^2 + p1^2 - p2^2) + L3*sin(theta)*((x2 - L2*cos(gamma + theta))^2 + (y2 - L2*sin(gamma + theta))^2 + p1^2 - p3^2))^2 - p1^2*(2*(y2 - L2*sin(gamma + theta))*(x1 - L3*cos(theta)) + 2*L3*sin(theta)*(x2 - L2*cos(gamma + theta)))^2 + ((x1 - L3*cos(theta))*((x2 - L2*cos(gamma + theta))^2 + (y2 - L2*sin(gamma + theta))^2 + p1^2 - p3^2) - (x2 - L2*cos(gamma + theta))*(L3^2*sin(theta)^2 + (x1 - L3*cos(theta))^2 + p1^2 - p2^2))^2;
 
-%staða1
+%stada1
 sol = bisect(fn,-pi,-0.5,0.5e-6);
 disp(sol);
-%staða2
+%stada2
 sol2 = bisect(fn,-0.5,0,0.5e-6);
 disp(sol2);
-%staða3
+%stada3
 sol3 = bisect(fn,0,0.4,0.5e-6);
 disp(sol3);
-%staða4
+%stada4
 sol4 = bisect(fn,0.4,0.5,0.5e-6);
 disp(sol4);
-%staða5
+%stada5
 sol5 = bisect(fn,0.5,2,0.5e-6);
 disp(sol5);
-%staða6
+%stada6
 sol6 = bisect(fn,2,pi,0.5e-6);
 disp(sol6);
 
