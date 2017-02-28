5d266ce563bcedcc0aed7e5485f4e87bac8c971c
alivarsdotttir <alexandralif@hotmail.com>
2017-02-28 10:40:36 +0000

    lidur2

--- s2/lidur2.m
+++ s2/lidur2.m
@@ -1,4 +1,4 @@
-function out = Jac(q,x)
+function out = lidur2(q,x)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -6,6 +6,22 @@ p0=5.2*10.^5;
 d=0.2;
 u=0.02;
 rho=998;
+
+q1=q(1,1);
+q2=q(2,1);
+q3=q(3,1);
+q4=q(4,1);
+q5=q(5,1);
+q6=q(6,1);
+q7=q(7,1);
+
+L1 = 100;
+L2 = x;
+L3 = 400 - 2*x;
+L4 = 75;
+L5 = x;
+L6 = 75;
+L7 = 50;
 
 f1=q1-q2-q6;
 f2=q2-q3-q4;
@@ -17,16 +33,16 @@ f7=L1*q1.^2+L6*q6.^2+L7*q7.^2-((pi.^2*d.^5)/8*u*rho)*p0;
 
 %J = [f1; f2; f3; f4; f5; f6; f7];
 
+syms q1 q2 q3 q4 q5 q6 q7 L1 L2 L3 L4 L5 L6 L7 f1 f2 f3 f4 f5 f6 f7;
 
-R1 = diff([f1, f2, f3, f4, f5, f6, f7], q1);
-R2 = diff([f1, f2, f3, f4, f5, f6, f7], q2);
-R3 = diff([f1, f2, f3, f4, f5, f6, f7], q3);
-R4 = diff([f1, f2, f3, f4, f5, f6, f7], q4);
-R5 = diff([f1, f2, f3, f4, f5, f6, f7], q5);
-R6 = diff([f1, f2, f3, f4, f5, f6, f7], q6);
-R7 = diff([f1, f2, f3, f4, f5, f6, f7], q7);
+R1 = diff([q1-q2-q6, q2-q3-q4, q3+q4-q5, q5+q6-q7, L3*q3.^2-L4*q4.^2, L2*q2.^2+L4*q4.^2+L5*q5.^2-L6*q6.^2, L1*q1.^2+L6*q6.^2+L7*q7.^2-((pi.^2*d.^5)/8*u*rho)*p0], q1);
+R2 = diff([q1-q2-q6, q2-q3-q4, q3+q4-q5, q5+q6-q7, L3*q3.^2-L4*q4.^2, L2*q2.^2+L4*q4.^2+L5*q5.^2-L6*q6.^2, L1*q1.^2+L6*q6.^2+L7*q7.^2-((pi.^2*d.^5)/8*u*rho)*p0], q2);
+R3 = diff([q1-q2-q6, q2-q3-q4, q3+q4-q5, q5+q6-q7, L3*q3.^2-L4*q4.^2, L2*q2.^2+L4*q4.^2+L5*q5.^2-L6*q6.^2, L1*q1.^2+L6*q6.^2+L7*q7.^2-((pi.^2*d.^5)/8*u*rho)*p0], q3);
+R4 = diff([q1-q2-q6, q2-q3-q4, q3+q4-q5, q5+q6-q7, L3*q3.^2-L4*q4.^2, L2*q2.^2+L4*q4.^2+L5*q5.^2-L6*q6.^2, L1*q1.^2+L6*q6.^2+L7*q7.^2-((pi.^2*d.^5)/8*u*rho)*p0], q4);
+R5 = diff([q1-q2-q6, q2-q3-q4, q3+q4-q5, q5+q6-q7, L3*q3.^2-L4*q4.^2, L2*q2.^2+L4*q4.^2+L5*q5.^2-L6*q6.^2, L1*q1.^2+L6*q6.^2+L7*q7.^2-((pi.^2*d.^5)/8*u*rho)*p0], q5);
+R6 = diff([q1-q2-q6, q2-q3-q4, q3+q4-q5, q5+q6-q7, L3*q3.^2-L4*q4.^2, L2*q2.^2+L4*q4.^2+L5*q5.^2-L6*q6.^2, L1*q1.^2+L6*q6.^2+L7*q7.^2-((pi.^2*d.^5)/8*u*rho)*p0], q6);
+R7 = diff([q1-q2-q6, q2-q3-q4, q3+q4-q5, q5+q6-q7, L3*q3.^2-L4*q4.^2, L2*q2.^2+L4*q4.^2+L5*q5.^2-L6*q6.^2, L1*q1.^2+L6*q6.^2+L7*q7.^2-((pi.^2*d.^5)/8*u*rho)*p0], q7);
 
-out = [transpose(R1), transpose(R2), transpose(R3), transpose(R4), transpose(R5), transpose(R6), transpose(R7)];
-
+out = transpose([(R1); (R2); (R3); (R4); (R5); (R6); (R7)]);
 end
 
