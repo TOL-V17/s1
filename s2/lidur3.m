f206f8286e1641871226b9b70af9af3ca99f8af6
alivarsdotttir <alexandralif@hotmail.com>
2017-02-28 12:09:32 +0000

    lidur3

--- s2/lidur3.m
+++ s2/lidur3.m
@@ -4,18 +4,18 @@ A = lidur2(q,x);
 disp(A)
 b = -(vigurfall(q,x));
 disp(b)
-s = A\b;
+s = vpa(A\b);
 tol = 0.5*10^-6;
 counter = 1;
 while(1)
     y = s+q;
     A = lidur2(y,x);
     b = -(vigurfall(y,x));
-    s = A\b;
-    counter = counter +1;
-    if (2/3)^counter > tol
+    s = vpa(A\b);
+    if (2/3).^counter > tol
         break
     end
+    counter = counter+1;
 end
-out = s;
+out = s+y;
 end
---
+++ s2/lidur3.m~
@@ -0,0 +1,6 @@
+function out = lidur3(q,x)
+
+A = lidur2(q,x);
+b = -(vigurfall(q,x));
+out = A\b;
+end
