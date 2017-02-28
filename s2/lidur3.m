2af63e86a200a497578fc111c4387846912bd543
rakels13 <rakels13@ru.is>
2017-02-28 11:56:20 +0000

    lidur3...

--- s2/lidur3.m
+++ s2/lidur3.m
@@ -1,6 +1,21 @@
 function out = lidur3(q,x)
 
 A = lidur2(q,x);
+disp(A)
 b = -(vigurfall(q,x));
-out = A\b;
+disp(b)
+s = A\b;
+tol = 0.5*10^-6;
+counter = 1;
+while(1)
+    y = s+q;
+    A = lidur2(y,x);
+    b = -(vigurfall(y,x));
+    s = A\b;
+    counter = counter +1;
+    if (2/3)^counter > tol
+        break
+    end
+end
+out = s;
 end
