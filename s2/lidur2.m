1a6fe723a5221848516d4e551469d6704b4337a4
alivarsdotttir <alexandralif@hotmail.com>
2017-02-28 11:03:09 +0000

    lidur2-final-final

--- s2/lidur2.m
+++ s2/lidur2.m
@@ -28,7 +28,15 @@ R6 = diff([q1-q2-q6, q2-q3-q4, q3+q4-q5, q5+q6-q7, L3*q3.^2-L4*q4.^2, L2*q2.^2+L
 R7 = diff([q1-q2-q6, q2-q3-q4, q3+q4-q5, q5+q6-q7, L3*q3.^2-L4*q4.^2, L2*q2.^2+L4*q4.^2+L5*q5.^2-L6*q6.^2, L1*q1.^2+L6*q6.^2+L7*q7.^2-((pi.^2*d.^5)/8*u*rho)*p0], q7);
 
 J = [R1; R2; R3; R4; R5; R6; R7];
-out = transpose(J);
+A = subs(J, q1, q(1,1));
+B = subs(A, q2, q(2,1));
+C = subs(B, q3, q(3,1));
+D = subs(C, q4, q(4,1));
+E = subs(D, q5, q(5,1));
+F = subs(E, q6, q(6,1));
+G = subs(F, q7, q(7,1));
+
+out = transpose(G);
 
 end
 
