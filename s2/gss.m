1c7dddd41a5c8b36a2a31ab3f0d62696fde4fd81
rakels13 <rakels13@ru.is>
2017-03-02 11:43:28 +0000

    lidur6

--- s2/gss.m
+++ s2/gss.m
@@ -6,7 +6,8 @@ function y=gss(f,a,b,k)
 g=(sqrt(5)-1)/2;
 x1 = a+(1-g)*(b-a);
 x2 = a+g*(b-a);
-f1=f(x1);f2=f(x2);
+f1=f(x1);
+f2=f(x2);
 for i=1:k
     if f1 < f2 % if f(x1) < f(x2), replace b with x2
         b=x2; x2=x1; x1=a+(1-g)*(b-a);
