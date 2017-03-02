7b214057bbaa2bd8ed044a5c3d817ecf584654a9
alivarsdotttir <alexandralif@hotmail.com>
2017-03-02 12:57:22 +0000

    lidur6 er naestum kominn

--- s2/lidur6.m
+++ s2/lidur6.m
@@ -3,12 +3,12 @@ g = (sqrt(5)-1)/2;
 k = log((0.5.*10^-2)/99)/log(g);
 q = ones(7,1)*0.1;
 u = f6(100);
-disp(u);
+%disp(u);
 a = 1;
 b = 199;
 M = [];
 for i = 1 : 7
-    f = u(i,1);
+    %f = u(i,1);
     disp(f);
     y=gss(a,b,k);
     M = [M;y];
